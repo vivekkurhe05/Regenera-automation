7708021feafa2c0520b6ee5da8181083ec47fcd7
Vivek Kurhe <vivek.kurhe@launchventures.co>
2022-07-19 16:16:38 +0530

    headless mode turned on

--- wdio.conf.js
+++ wdio.conf.js
@@ -65,6 +65,15 @@ exports.config = {
       //
       browserName: "chrome",
       acceptInsecureCerts: true,
+      "goog:chromeOptions": {
+        args: [
+          "--headless",
+          "--disable-gpu",
+          "-window-size=1400,1400",
+          "no-sandbox",
+          "--disable-dev-shm-usage",
+        ],
+      },
       // If outputDir is provided WebdriverIO can capture driver session logs
       // it is possible to configure which logTypes to include/exclude.
       // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
